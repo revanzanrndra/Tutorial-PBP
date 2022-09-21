a280f860d6c6a1dba9180f52b378ecd0c8bb8f0e
TM Revanza <revanzanrndra@gmail.com>
2022-09-21 23:16:16 +0700

    Tutorial 3 Kelar

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from wishlist.views import show_wishlist
-from wishlist.views import show_xml
-from wishlist.views import show_xml_by_id
+from wishlist.views import show_wishlist, show_xml, \
+    show_xml_by_id, register, login_user, logout_user
 
 app_name = 'wishlist'
 
@@ -9,4 +8,7 @@ urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
     path('xml/', show_xml, name='show_xml'),
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
